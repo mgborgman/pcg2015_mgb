8fab62c148a7ddb1c33c1be5992e639bcab49fcd
Matt Borgman <mgborgman@gmail.com>
2015-03-30 20:04:45 -0700

    tango with django

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
 
-admin.site.register(Category)
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page)
 
-
-admin.site.register(Category)
-admin.site.register(Page)
 
 
