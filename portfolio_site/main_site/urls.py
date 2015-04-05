099321cb126af957d1f08a194f0ad6c30470f94d
Matt Borgman <mgborgman@gmail.com>
2015-04-05 14:16:53 -0700

    update

--- portfolio_site/main_site/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-from main_site import views
-
-urlpatterns = patterns('',
-                       url(r'^contact/', include('contact_form.urls')),
-                       url(r'^index/', views.index, name='index')
-                       )
--- portfolio_site/main_site/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    context_dict = {'boldmessage': 'TEST'}
-    return render(request, 'index.html', context_dict)
-
-
-
