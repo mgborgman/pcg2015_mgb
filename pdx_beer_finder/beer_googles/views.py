754787dcb396b080ebee0152773e6dc8fb35bfb6
Matt Borgman <mgborgman@gmail.com>
2015-04-07 20:39:27 -0700

    beer/bar finder app

--- pdx_beer_finder/beer_googles/views.py
+++ pdx_beer_finder/beer_googles/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from models import Bar
+from models import Bar, Beer
+
 
 # Create your views here.
-def index(request):
-    bars = Bar.objects.all()
 
-    context_dict = {'bar': bars}
+def index(request):
+    beer_results = None
+    bar_results = None
+    if request.POST:
+
+        query = request.POST['bar_search']
+        bar_results = Bar.objects.filter(name__contains=query)
+
+        query2 = request.POST['beer_search']
+        beer_results = Beer.objects.filter(name__contains=query2)
+
+
+
+
+
+
+
+
+
+
+    context_dict = {'bar_results': bar_results, 'beer_results': beer_results}
     return render(request, 'index.html', context_dict)
