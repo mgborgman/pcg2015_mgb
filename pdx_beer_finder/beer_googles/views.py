62ebf74c214e3ea1eb664f26c032246bf6406a21
Matt Borgman <mgborgman@gmail.com>
2015-04-08 20:58:55 -0700

    beer search app

--- pdx_beer_finder/beer_googles/views.py
+++ pdx_beer_finder/beer_googles/views.py
@@ -5,12 +5,16 @@ from models import Bar, Beer
 # Create your views here.
 
 def index(request):
-    beer_results = None
+    bar_list = Bar.objects.all()
+    beer_list = Beer.objects.all()
     bar_results = None
-    if request.POST:
+    beer_results = None
+    if 'bar_submit' in request.POST:
 
         query = request.POST['bar_search']
         bar_results = Bar.objects.filter(name__contains=query)
+
+    elif 'beer_submit' in request.POST:
 
         query2 = request.POST['beer_search']
         beer_results = Beer.objects.filter(name__contains=query2)
@@ -24,5 +28,5 @@ def index(request):
 
 
 
-    context_dict = {'bar_results': bar_results, 'beer_results': beer_results}
+    context_dict = {'bar_results': bar_results, 'beer_results': beer_results, 'bar_list': bar_list, 'beer_list': beer_list}
     return render(request, 'index.html', context_dict)
